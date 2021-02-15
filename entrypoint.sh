f42f0630ddb0115053f43a88e8a549306242e946
Yuki Kikuchi <24319042+bclswl0827@users.noreply.github.com>
2021-02-16 01:38:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 WEBSDR_PATH=$(find /usr/bin/websdr -name "dist*")
 WEBSDR_BIN=$(find ${WEBSDR_PATH} -name "websdr*" -type f | head -n1)
-chmod 755 ${WEBSDR_BIN}
+mkdir -p ${WEBSDR_PATH}/pub2/tmp
+chmod 755 ${WEBSDR_PATH}/pub2/tmp \
+          ${WEBSDR_BIN}
 cd ${WEBSDR_PATH}; ${WEBSDR_BIN}
 
