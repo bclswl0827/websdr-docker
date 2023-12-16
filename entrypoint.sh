90bc7f338ab2d4014bbbd7c87327c9c402bf0f36
Seunghun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-12-16 17:45:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 WEBSDR_PATH=$(find /usr/bin/websdr -name "dist*")
-WEBSDR_BIN=$(find ${WEBSDR_PATH} -name "websdr*" -type f | head -n1)
+WEBSDR_BIN=$(find ${WEBSDR_PATH} -name "websdr" -type f | head -n1)
+WEBSDR_ARCH=$(if [ `uname -m` == "x86_64" ]; then echo "64"; elif [ `uname -m` == "armv7l" ]; then echo "-rpi"; fi)
 mkdir -p ${WEBSDR_PATH}/pub2/tmp
 chmod 755 ${WEBSDR_PATH}/pub2/tmp \
-          ${WEBSDR_BIN}
-cd ${WEBSDR_PATH}; ${WEBSDR_BIN}
-
+          ${WEBSDR_BIN}${WEBSDR_ARCH}
+cd ${WEBSDR_PATH}; ${WEBSDR_BIN}${WEBSDR_ARCH}
